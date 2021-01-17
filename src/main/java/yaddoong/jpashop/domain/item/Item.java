02acfce61941cd10f95aef3a81dbd10f74978121
KimJoongKi <thoug12@naver.com>
2021-01-17 16:44:17 +0900

    엔티티 클래스 개발2

--- src/main/java/yaddoong/jpashop/domain/item/Item.java
+++ src/main/java/yaddoong/jpashop/domain/item/Item.java
@@ -2,8 +2,11 @@ package yaddoong.jpashop.domain.item;
 
 import lombok.Getter;
 import lombok.Setter;
+import yaddoong.jpashop.domain.Category;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -19,5 +22,8 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
